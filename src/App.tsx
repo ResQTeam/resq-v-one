4fcfb5f6c8216235900b4e31bca7d5e0e2b334c5
talentlessDeveloper <kareemope52@gmail.com>
2023-07-11 19:35:34 +0100

    deleted node modules and reinstalled due to deps conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-function App() {
-  return (
-    <div className="bg-resq-brand-main text-resq-light-100 text-center text-4xl">
-      <h1 className="font-poppins text-h1">Welcome</h1>
-      <p className="font-volkhov text-h2">Let's test</p>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
