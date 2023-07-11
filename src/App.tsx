42fe69605845249beb5495dadcfa8b8487247d18
talentlessDeveloper <kareemope52@gmail.com>
2023-07-11 16:27:37 +0100

    tested on tailwind style on app and installed react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="text-red-500 text-4xl">Welcome</div>;
+  return (
+    <div className="bg-resq-brand-main text-resq-light-100 text-center text-4xl">
+      <h1 className="font-poppins text-h1">Welcome</h1>
+      <p className="font-volkhov text-h2">Let's test</p>
+    </div>
+  );
 }
 
 export default App;
