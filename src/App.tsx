319b1e722cf23c2b94cb8d22d862ab99b2b5475b
talentlessDeveloper <kareemope52@gmail.com>
2023-07-12 14:54:28 +0100

    added routes for home and about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import PagesLayout from './components/shared/PagesLayout';
+import Home from './pages/Home';
+import About from './pages/About';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      {' '}
+      <Routes>
+        <Route element={<PagesLayout />} path="/">
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
