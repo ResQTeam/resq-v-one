22304dd3f3d13edb9324dd929fe65ed55e8d77c7
talentlessDeveloper <kareemope52@gmail.com>
2023-07-15 20:06:17 +0100

    passed setOpenMenu as props to header menu component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
+import Emergency from '../shared/Emergency';
+import HeaderMenu from './HeaderMenu';
 import MenuButton from './MenuButton';
-import Phone from '../shared/svg/Phone';
 
-const links = [
+export const links = [
   {
     name: 'home',
     path: '/',
@@ -30,14 +31,14 @@ const links = [
 const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
   return (
-    <header className="bg-white">
+    <header className="bg-white overflow-x-clip">
       <nav className="max-w-7xl mx-auto py-7 px-6 flex justify-between items-center">
         <Link to="/" className="hidden md:block">
           <p className="text-resq-dark-400 text-[2.125rem]/[1.5rem] font-volkhov">
             ResQ
           </p>
         </Link>
-        <div className="block md:hidden">
+        <div className="block md:hidden relative z-[6]">
           <MenuButton openMenu={openMenu} setOpenMenu={setOpenMenu} />
         </div>
 
@@ -61,19 +62,11 @@ const Header = () => {
             );
           })}
         </ul>
-
-        <div className="flex items-center gap-x-1 md:gap-x-3">
-          <div className="bg-resq-brand-main w-[29px] h-[27px] md:w-[59px] md:h-[52px] flex justify-center items-center rounded-md md:rounded-[10px]">
-            <Phone className="w-5 h-5  md:w-[27px] md:h-[35px]" />
-          </div>
-          <div className="">
-            <p className="text-[0.625rem] md:text-base">Emergency Call</p>
-            <p className="text-resq-dark-400 font-bold text-xs md:text-[1.375rem]/[1.5rem]">
-              777 657 0000
-            </p>
-          </div>
+        <div className="relative z-[6]">
+          <Emergency textColor="black" />
         </div>
       </nav>
+      <HeaderMenu openMenu={openMenu} setOpenMenu={setOpenMenu} />
     </header>
   );
 };
