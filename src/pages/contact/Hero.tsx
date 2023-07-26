2f793269657822c3f1908c40e320ce5bba49e006
talentlessDeveloper <kareemope52@gmail.com>
2023-07-26 08:42:05 +0100

    contact page done

---
+++ src/pages/contact/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <section className="mt-[3.125rem]">
+      <div className=" space-y-2 bg-resq-brand-main py-20 text-center  md:py-32">
+        <h1 className="text-[0.625rem]/[0.96875rem] font-bold uppercase tracking-[1.5px] text-white md:text-[0.9375rem]/[1.45rem]">
+          Get In Touch with Us
+        </h1>
+        <h2 className=" text-xl/6 font-bold text-white md:text-[3.5rem]/[120%] ">
+          We Are Ready To Assist <br /> You 24/7
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,15 @@
+import Form from './Form';
+import Help from './Help';
+import Hero from './Hero';
+
+const Contact = () => {
+  return (
+    <>
+      <Hero />
+      <Help />
+      <Form />
+    </>
+  );
+};
+
+export default Contact;
