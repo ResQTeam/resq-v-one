2b28fb8d6bf095c5b57d8c8a298202eb0d3ded00
talentlessDeveloper <kareemope52@gmail.com>
2023-09-10 15:41:54 +0100

    fixed unused id declaration and Number type

--- src/pages/Service/FAQ.tsx
+++ src/pages/Service/FAQ.tsx
@@ -1,19 +1,23 @@
 import { useState } from 'react';
-import {AiOutlineDown,AiOutlineUp} from 'react-icons/ai'
+import { AiOutlineDown, AiOutlineUp } from 'react-icons/ai';
 
 interface FAQProps {
-  id: Number;
   question: string;
   answer: string;
 }
 
-const FAQ: React.FC<FAQProps> = ({ id, question, answer }) => {
+const FAQ: React.FC<FAQProps> = ({ question, answer }) => {
   const [isAnswerShowing, setAnswerShowing] = useState(false);
 
   return (
-    <article className="md:p-6 p-4 rounded-lg cursor-pointer border-solid border mb-4" onClick={() => setAnswerShowing((prev) => !prev)}>
-      <div className='flex justify-between gap-8 '>
-        <h2 className='text-resq-dark-400 font-semibold inline-block text-[0.7255rem] text-600 md:text-lg'>{question}</h2>
+    <article
+      className="mb-4 cursor-pointer rounded-lg border border-solid p-4 md:p-6"
+      onClick={() => setAnswerShowing((prev) => !prev)}
+    >
+      <div className="flex justify-between gap-8 ">
+        <h2 className="text-600 inline-block text-[0.7255rem] font-semibold text-resq-dark-400 md:text-lg">
+          {question}
+        </h2>
         <button className="bg-transparent text-xl">
           {isAnswerShowing ? <AiOutlineUp /> : <AiOutlineDown />}
         </button>
